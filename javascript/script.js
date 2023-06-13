86a520b21dc6515341d35d393c90121a0af7dfb4
michaelangelesz <125311967+michaelangelesz@users.noreply.github.com>
2023-06-13 11:55:55 -0400

    add sound to my card flip

--- javascript/script.js
+++ javascript/script.js
@@ -1,7 +1,11 @@
-const cards = document.querySelectorAll(".memory-card");
+const cards = document.querySelectorAll('.memory-card');
+const flipSound = new Audio('assets/cardflip.mp3');
+flipSound.volume = 0.5;
+flipSound.playbackRate = 2;
 
 function flipCard() {
-this.classList.toggle("flip");
+this.classList.toggle('flip');
+flipSound.play();
 }
 
-cards.forEach(card => card.addEventListener("click", flipCard));
+cards.forEach(card => card.addEventListener('click', flipCard));
