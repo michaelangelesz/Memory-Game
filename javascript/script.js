0790c59a2db16be767991d68991922933355464b
michaelangelesz <125311967+michaelangelesz@users.noreply.github.com>
2023-06-13 19:10:30 -0400

    add lockBoard() and resetBoard()
    lockBoard() and related code prevents clicking more than 2 cards at a time
    
    resetBoard() works with `if (this === firstCard) return;` in flipCard(). this prevents double clicking on the same card.

--- javascript/script.js
+++ javascript/script.js
@@ -8,23 +8,26 @@ unflipSound.playbackRate = 2;
 
 // declare variables
 let hasFlippedCard = false;
+let lockBoard = false;
 let firstCard, secondCard;
 
 function flipCard() {
-  flipSound.play();
+  // locks board so you can't click more than 2 cards at a time
+  if (lockBoard) return;
+  // prevents double clicking on same card
+  if (this === firstCard) return;
   this.classList.add("flip");
-
+  flipSound.play();
+  
   if (!hasFlippedCard) {
     // first click
     hasFlippedCard = true;
     firstCard = this;
-
     return;
   }
-  // second click
-  hasFlippedCard = false;
-  secondCard = this;
 
+  // second click
+  secondCard = this;
   checkForMatch();
 }
 
@@ -37,14 +40,24 @@ function checkForMatch() {
   function disableCards() {
     firstCard.removeEventListener("click", flipCard);
     secondCard.removeEventListener("click", flipCard);
+    resetBoard();
   }
 
   function unflipCards() {
+    // lock the board
+    lockBoard = true;
     setTimeout(() => {
       unflipSound.play();
       firstCard.classList.remove("flip");
       secondCard.classList.remove("flip");
+      // unlocks board after 1.5 seconds
+      resetBoard();
     }, 1500);
+  }
+
+  function resetBoard() {
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
   }
 }
 
