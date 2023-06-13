a4b0db1f343a7d3f7540a9bb6193d17034442ddb
michaelangelesz <125311967+michaelangelesz@users.noreply.github.com>
2023-06-13 14:42:07 -0400

    begin adding match variables, console.log
    add screencap

--- javascript/script.js
+++ javascript/script.js
@@ -3,9 +3,18 @@ const flipSound = new Audio('assets/cardflip.mp3');
 flipSound.volume = 0.5;
 flipSound.playbackRate = 2;
 
+let hasFlippedCard = false;
+let firstCard, secondCard;
+
 function flipCard() {
-this.classList.toggle('flip');
-flipSound.play();
+    flipSound.play();
+this.classList.add('flip');
+if (!hasFlippedCard) {
+    hasFlippedCard = true;
+    firstCard = this;
+
+    console.log({hasFlippedCard, firstCard});
+}
 }
 
 cards.forEach(card => card.addEventListener('click', flipCard));
