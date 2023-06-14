2e727074920521b91cb37ea108a0b74887f87d39
michaelangelesz <125311967+michaelangelesz@users.noreply.github.com>
2023-06-13 21:19:18 -0400

    add shuffle function for page load

--- javascript/script.js
+++ javascript/script.js
@@ -61,4 +61,11 @@ function checkForMatch() {
   }
 }
 
+(function shuffle() {
+    cards.forEach(card => {
+        let randomPos = Math.floor(Math.random() * 18);
+        card.style.order = randomPos;
+    });
+})();
+
 cards.forEach((card) => card.addEventListener("click", flipCard));
