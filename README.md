72e0b082d40e5b2d83b693ea2c5c04dde8ac8782
michaelangelesz <125311967+michaelangelesz@users.noreply.github.com>
2023-06-17 18:35:42 -0400

    fixed a way to run a WIN finction
    added count++; to disableCards() function, to count how many times it disables (matches) cards. at 9 matches, win game!

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ The Rider Waite Smith Tarot Deck is a classic tarot deck first published in 1909
 
 In this project, I use the Rider Waite Smith Tarot Deck to create a memory game that challenges players to match pairs of cards and improve their memory skills. The cards are randomly shuffled and displayed face down on the screen. The player must flip them over, one by one, to find the matching pairs in the alloted time. If they succeed, they win, with the option to play again. If they fail, they lose, again with the option to try again.
 
-I acknowledge the creators and publishers of the Rider Waite Smith Tarot Deck for their contribution to the world of tarot and divination, and I use their work with respect and appreciation.
+I acknowledge the creators and publishers of the Rider Waite Smith Tarot Deck for their contribution to the world of tarot and divination, and I use their work with respect, appreciation, and reverence.
 
 - Title: Rider Waite Smith Tarot Deck
 - Creator: Arthur Edward Waite (author) and Pamela Colman Smith (artist)
--- javascript/script.js
+++ javascript/script.js
@@ -8,10 +8,10 @@ const unflipSound = new Audio("assets/unflip.mp3");
 unflipSound.volume = 0.3;
 unflipSound.playbackRate = 2;
 
+
 // declare variables
 let countDown;
-let timeSecond = 300;
-// let matchedCards = 0; // counter variable for flipped cards
+let timeSecond = 30;
 
 displayTime(timeSecond);
 
@@ -24,12 +24,6 @@ function startTimer() {
     if (seconds <= 0 || seconds < 1) {
       endTime();
       clearInterval(countDown);
-    // //   setTimeout(() => {
-    // //     alert("You lose! 😭😭😭 Try again!");
-    // //   }, 333);
-    // } else if (matchedCards === 18) {
-    //     winGame();
-    //     clearInterval(countDown);
     }
   }, 1000);
 }
@@ -38,17 +32,6 @@ function displayTime(second) {
   const min = Math.floor(second / 60);
   const sec = Math.floor(second % 60);
   timeH.innerHTML = `${min < 10 ? "0" : ""}${min}:${sec < 10 ? "0" : ""}${sec}`;
-}
-
-function endTime() {
-  timeH.innerHTML = `😵‍💫 GAME OVER 😵‍💫`;
-  lockBoard = true;
-}
-
-function winGame() {
-
-    timeH.innerHTML = `🤩 YOU WIN! 🤩`;
-    console.log("win");
 }
 
 let intialFlip = false;
@@ -79,14 +62,9 @@ function flipCard() {
   // second click
   secondCard = this;
   checkForMatch();
-
-// //   // increment the counter variable
-//   matchedCards++;
-//   if (allCardsFlipped(matchedCards === 18)) {
-//     stopTimer();
-//     //alert("🎉Congratulations!🎉 You matched all the cards!!👏👏👏");
-//   }
 }
+
+let count = 0;
 
 function checkForMatch() {
   // ternary operator is if/else statement in one line
@@ -97,6 +75,11 @@ function checkForMatch() {
   function disableCards() {
     firstCard.removeEventListener("click", flipCard);
     secondCard.removeEventListener("click", flipCard);
+    count++;
+    console.log("Function has been called " + count + " times.");
+    if (count === 9) {
+        winGame();
+    }
     resetBoard();
   }
 
@@ -118,36 +101,32 @@ function checkForMatch() {
   }
 }
 
-// function allCardsFlipped() {
-//     let allFlipped = true;
-//     cards.forEach((card) => {
-//       if (!card.classList.contains("flip")) {
-//         allFlipped = false;
-//       }
-//     });
-//     if (allFlipped) {
-//       clearInterval(timerInterval); // clear the interval when all cards are flipped
-//       alert("Congratulations! You have matched all the cards!");
-//     }
-//   }
+function allCardsFlipped() {
+    let allFlipped = true;
+    cards.forEach((card) => {
+      if (!card.classList.contains("flip")) {
+        allFlipped = false;
+      }
+    });
+    if (allFlipped) {
+      clearInterval(timerInterval); // clear the interval when all cards are flipped
+      alert("Congratulations! You have matched all the cards!");
+    }
+  }
 
-(function shuffle() {
-  cards.forEach((card) => {
-    let randomPos = Math.floor(Math.random() * 18);
-    card.style.order = randomPos;
-  });
-})();
+function endTime() {
+  timeH.innerHTML = `😵‍💫 GAME OVER 😵‍💫`;
+  lockBoard = true;
+}
 
-// function stopTimer() {
-//   clearInterval(countDown);
-//   timeH.innerHTML = `🤩 YOU WIN! 🤩`;
-// }
+function winGame() {
+    
+    timeH.innerHTML = `🤩 YOU WIN! 🤩`;
+    console.log("Count has reached 9.");
+}
 
 function newGame() {
   location.reload();
 }
 
 cards.forEach((card) => card.addEventListener("click", flipCard));
-
-// // // stop the timer when all cards are matched
-// cards[cards.length - 0].addEventListener("matchedCards === 18", stopTimer);
